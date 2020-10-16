bb2bacd6e4a30e7969c0c119e1596d3397036516
Simen <simesal@stud.ntnu.no>
2020-10-16 11:27:33 +0200

    Added visual support for the usergenerated data frontend. Still need to hook it up to backend.

--- src/components/CountryDisplay.tsx
+++ src/components/CountryDisplay.tsx
@@ -4,8 +4,8 @@ import Country from './Country'
 export default function CountryDisplay() {
     return (
         <div className="CountryDisplay">
-            <Country countryID="AX"/>
-            <Country countryID="SE"/>
+            <Country countryID="AX" wish={true} flag={false}/>
+            <Country countryID="SE" wish={false} flag={false}/>
         </div>
     )
 }
