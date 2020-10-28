086de259c5ff5bb2dc33fa8633e86585587ee3f3
Hanna <hanntorj@stud.ntnu.no>
2020-10-28 16:39:47 +0100

    added url to fit filters in countryfetch

--- src/Fetch.tsx
+++ src/Fetch.tsx
@@ -1,24 +1,43 @@
-import { GetCountryMoreInfo, GetCountryList, User, Filters } from "./Interfaces";
+import {
+  GetCountryMoreInfo,
+  GetCountryList,
+  User,
+  Filters,
+} from "./Interfaces";
 
 // const url = "https://restcountries.eu/rest/v2/";
 const url = "http://localhost:8080/api/";
 
-
-
-export async function getCountryList({
-  sort,
-  searchString,
-  handleResponse,
-  limit,
-  skip,
-}: GetCountryList, filter: Filters) {
-  let fetchUrl = url + "?search=" + `${searchString}&limit=${limit}&skip=${skip}&sort=${sort}`
-  if (filter.regions) {
-    for (const region in filter.regions) {
-      fetchUrl+= '&region=' + filter.regions[region]
+export async function getCountryList(
+  { sort, searchString, handleResponse, limit, skip }: GetCountryList,
+  filter: Filters
+) {
+  let fetchUrl =
+    url +
+    "?search=" +
+    `${searchString}&limit=${limit}&skip=${skip}&sort=${sort}`;
+  if (filter.pop) {
+    fetchUrl += "&pop=" + filter.pop;
+    if (filter.popGreater) {
+      fetchUrl += "&popString=gt";
+    } else {
+      fetchUrl += "&popString=lt";
     }
   }
-  const response = await fetch(fetchUrl)
+  if (filter.area) {
+    fetchUrl += "&area=" + filter.area;
+    if (filter.areaGreater) {
+      fetchUrl += "&areaString=gt";
+    } else {
+      fetchUrl += "&areaString=lt";
+    }
+  }
+  if (filter.regions) {
+    for (const region in filter.regions) {
+      fetchUrl += "&region=" + filter.regions[region];
+    }
+  }
+  const response = await fetch(fetchUrl);
   const data = await response.json();
   handleResponse(data);
 }
@@ -27,40 +46,41 @@ export async function getCountryMoreInfo({
   alpha2Code,
   handleResponse,
 }: GetCountryMoreInfo) {
-  const response = await fetch(
-    url+'country/' + `${alpha2Code}`
-  )
+  const response = await fetch(url + "country/" + `${alpha2Code}`);
   const data = await response.json();
-  
+
   handleResponse(data[0]);
 }
 
 export async function requestUserID(handleResponse: (data: User) => void) {
-  const response = await fetch(url + "requestUserID")
-  const data = await response.json()
+  const response = await fetch(url + "requestUserID");
+  const data = await response.json();
 
-  handleResponse(data)
+  handleResponse(data);
 }
 
-export async function getUserData(handleResponse: (data: User) => void, userID: string) {
-  const response = await fetch(url + "getUserData/"+ userID)
-  const data = await response.json()
+export async function getUserData(
+  handleResponse: (data: User) => void,
+  userID: string
+) {
+  const response = await fetch(url + "getUserData/" + userID);
+  const data = await response.json();
 
-  handleResponse(data)
+  handleResponse(data);
 }
 
 export async function userRemoveFlag(alpha: string, userID: string) {
-  await fetch(url + "userRemoveFlag/" + userID + "/" + alpha)
+  await fetch(url + "userRemoveFlag/" + userID + "/" + alpha);
 }
 
 export async function userRemoveWish(alpha: string, userID: string) {
-  await fetch(url + "userRemoveWish/" + userID + "/" + alpha)
+  await fetch(url + "userRemoveWish/" + userID + "/" + alpha);
 }
 
 export async function userAddFlag(alpha: string, userID: string) {
-  await fetch(url + "userAddFlag/" + userID + "/" + alpha)
+  await fetch(url + "userAddFlag/" + userID + "/" + alpha);
 }
 
 export async function userAddWish(alpha: string, userID: string) {
-  await fetch(url + "userAddWish/" + userID + "/" + alpha)
+  await fetch(url + "userAddWish/" + userID + "/" + alpha);
 }
