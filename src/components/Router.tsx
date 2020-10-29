fd8e578d20f3cf216de8df62acfe2c3f888046c0
Hanna <hanntorj@stud.ntnu.no>
2020-10-29 19:36:30 +0100

    divbox router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CountryDisplay from "./CountryDisplay";
-import SearchBar from "./SearchBar"
-import CountryDisplayMoreInfo from "./CountryDisplayMoreInfo"
+import SearchBar from "./SearchBar";
+import CountryDisplayMoreInfo from "./CountryDisplayMoreInfo";
 import WishButton from "./WishButton";
 import FilterDisplay from "./FilterDisplay";
-
-
 
 export default function Router() {
   return (
@@ -14,8 +12,10 @@ export default function Router() {
       <div className="router">
         <Switch>
           <Route exact path="/">
-            <SearchBar />
-            <WishButton/>
+            <div className="TopBar">
+              <SearchBar />
+              <WishButton />
+            </div>
             <div className="FilterAndCountry">
               <FilterDisplay />
               <CountryDisplay />
