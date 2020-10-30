dcdd81c1b66c6dff7d3829540d5bb96f1b4af6b9
Simen <simesal@stud.ntnu.no>
2020-10-30 17:59:02 +0100

    Added test to check that userDisplayButtons renders correctly and that the app renders

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import ReactDOM from 'react-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('App renders', () => {
+  //const { getByText } = render(<App />);
+  //const linkElement = getByText(/learn react/i);
+  //expect(linkElement).toBeInTheDocument();
+  const div = document.createElement("div")
+  ReactDOM.render(<App></App>,div)
 });
